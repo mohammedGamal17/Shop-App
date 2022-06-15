1fccff637e3259e7ce78f1b2e49f9150c0deda04
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-15 19:07:59 +0200

    Update login_state.dart

--- lib/modules/login/login_cubit/login_state.dart
+++ lib/modules/login/login_cubit/login_state.dart
@@ -1,5 +1,7 @@
-import 'package:shop_app/models/shop_login_model.dart';
+
 import 'package:shop_app/modules/Login/login_cubit/login_state.dart';
+
+import '../../../models/login_molel/shop_login_model.dart';
 
 abstract class LoginStates {}
 
