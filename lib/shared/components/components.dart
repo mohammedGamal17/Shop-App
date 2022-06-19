b427e4be1fefff294a5aad76c67d8a198049af05
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-19 10:11:26 +0200

    Update components.dart

--- lib/shared/components/components.dart
+++ lib/shared/components/components.dart
@@ -165,5 +165,5 @@ ScaffoldFeatureController<SnackBar, SnackBarClosedReason> snack(
 }
 
 Widget stackText(context, {required String text}) {
-  return Text(text, style: Theme.of(context).textTheme.overline,maxLines: 1,overflow:TextOverflow.ellipsis ,);
+  return Text(text, style: Theme.of(context).textTheme.overline,maxLines: 1,overflow:TextOverflow.ellipsis ,textAlign: TextAlign.center,);
 }
