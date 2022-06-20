3276caac446d32f8a2e726826ad0785ee642ef0b
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-20 10:34:20 +0200

    Create data.dart

---
+++ lib/models/change_fav_model/data.dart
@@ -0,0 +1,26 @@
+import 'package:shop_app/models/change_fav_model/product.dart';
+
+class Data {
+  int? id;
+  Product? product;
+
+  Data({this.id, this.product});
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      id: json['id'],
+      product:
+          json['product'] != null ? Product.fromJson(json['product']) : null,
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = <String, dynamic>{};
+    data['id'] = id;
+    final product = this.product;
+    if (product != null) {
+      data['product'] = product.toJson();
+    }
+    return data;
+  }
+}
