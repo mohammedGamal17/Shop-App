97d563aa81e789d69d4a5f03f8cbf6e26e579e76
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-22 11:49:23 +0200

    Create data_x.dart

---
+++ lib/models/get_fav_model/data_x.dart
@@ -0,0 +1,16 @@
+import '../change_fav_model/product.dart';
+
+class DataX {
+  int? id;
+  Product? product;
+
+  DataX({this.id, this.product});
+
+  factory DataX.fromJson(Map<String, dynamic> json) {
+    return DataX(
+      id: json['id'],
+      product:
+          json['product'] != null ? Product.fromJson(json['product']) : null,
+    );
+  }
+}
