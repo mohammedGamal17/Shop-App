c472db59ebcc43b285e7476969ec7a938803dd6b
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-24 05:35:06 +0200

    Update favourite_screen.dart

--- lib/modules/home_screens/favourite/favourite_screen.dart
+++ lib/modules/home_screens/favourite/favourite_screen.dart
@@ -2,13 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hexcolor/hexcolor.dart';
 
-
-import '../../../models/get_fav_model/data_x.dart';
-
+import '../../../models/get_fav_model/get_favorite_data.dart';
 import '../../../shared/components/components.dart';
 import '../../../shared/cubit/cubit.dart';
 import '../../../shared/cubit/states.dart';
 import '../../../shared/styles/colors.dart';
+import '../../Login/login.dart';
 
 class FavouriteScreen extends StatelessWidget {
   const FavouriteScreen({Key? key}) : super(key: key);
@@ -22,24 +21,23 @@ class FavouriteScreen extends StatelessWidget {
         builder: (context, state) {
           AppCubit cubit = AppCubit.get(context);
           return Scaffold(
-            body: state is !GetFavLoadingState
+            body: state is ! GetFavLoadingState
                 ? ListView.separated(
                     physics: const BouncingScrollPhysics(),
                     itemBuilder: (context, index) => itemBuilder(
-                        cubit.getFavModel!.data!.data![index], context),
+                        cubit.getFavModel!.data!.data[index], context),
                     separatorBuilder: (context, index) =>
                         const SizedBox(height: 0.0),
-                    itemCount: cubit.getFavModel!.data!.data!.length,
+                    itemCount: cubit.getFavModel!.data!.data.length,
                   )
-                : circularProgressIndicator(),
+                : circularProgressIndicator()
           );
         },
       ),
     );
   }
 
-  Widget itemBuilder(DataX model, context) {
-
+  Widget itemBuilder(FavouriteData model, context) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
       child: Container(
@@ -65,21 +63,21 @@ class FavouriteScreen extends StatelessWidget {
                 alignment: AlignmentDirectional.bottomStart,
                 children: [
                   Image(
-                    image: NetworkImage('${model.product?.image}'),
+                    image: NetworkImage('${model.product!.image}'),
                     height: 125.0,
                     width: 125.0,
                     fit: BoxFit.fill,
                   ),
                   Row(
                     children: [
-                      model.product?.discount != 0
+                      model.product!.discount != 0
                           ? Container(
                               color: Colors.red,
                               child: Padding(
                                 padding:
                                     const EdgeInsets.symmetric(horizontal: 7.0),
                                 child: Text(
-                                  '% ${model.product?.discount}',
+                                  '% ${model.product!.discount}',
                                   style: const TextStyle(
                                     color: Colors.white,
                                     fontSize: 10.0,
@@ -101,7 +99,7 @@ class FavouriteScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Text(
-                      '${model.product?.name}',
+                      '${model.product!.name}',
                       style: TextStyle(
                         color: HexColor('0077B6'),
                         fontFamily: 'Changa',
@@ -113,7 +111,7 @@ class FavouriteScreen extends StatelessWidget {
 
                     ///if discount = 0 hide it
                     Text(
-                      'EGP ${model.product?.oldPrice}',
+                      'EGP ${model.product!.oldPrice}',
                       style: TextStyle(
                         color: HexColor('0077B6'),
                         fontFamily: 'Changa',
@@ -123,7 +121,7 @@ class FavouriteScreen extends StatelessWidget {
                     const Spacer(),
                     Row(
                       children: [
-                        if (model.product?.discount != 0)
+                        if (model.product!.discount != 0)
                           Text(
                             'EGP 2000',
                             style: TextStyle(
@@ -136,20 +134,22 @@ class FavouriteScreen extends StatelessWidget {
                         const Spacer(),
                         IconButton(
                           onPressed: () {
-                             AppCubit.get(context).postFavData(model.id!);
+                            AppCubit.get(context)
+                                .postFavData(model.product!.id);
                           },
-                          icon: AppCubit.get(context)
-                                      .favMap[model.product?.id] ==
-                                  null
-                              ? Icon(
-                                  AppCubit.get(context).icon = Icons.favorite,
-                                  color: HexColor('0077B6'),
-                                )
-                              : Icon(
-                                  AppCubit.get(context).icon =
-                                      Icons.favorite_border_outlined,
-                                  color: HexColor('0077B6'),
-                                ),
+                          icon:
+                              AppCubit.get(context).favMap[model.product!.id] !=
+                                      true
+                                  ? Icon(
+                                      AppCubit.get(context).favIcon =
+                                          Icons.favorite,
+                                      color: HexColor('0077B6'),
+                                    )
+                                  : Icon(
+                                      AppCubit.get(context).favIcon =
+                                          Icons.favorite_border_outlined,
+                                      color: HexColor('0077B6'),
+                                    ),
                         ),
                       ],
                     ),
