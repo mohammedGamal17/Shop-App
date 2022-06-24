f89a54da5dc17ad6df6d2babd0be8a93fad25f7a
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-24 05:35:04 +0200

    Update product.dart

--- lib/models/home_model/product.dart
+++ lib/models/home_model/product.dart
@@ -1,39 +1,25 @@
 class Product {
   String? description;
   dynamic discount;
-  int? id;
+  late int id;
   String? image;
   List<String>? images;
   bool? inCart;
-  bool? inFavorites;
+  late bool inFavorites;
   String? name;
   dynamic oldPrice;
   dynamic price;
 
-  Product(
-      {this.description,
-      this.discount,
-      this.id,
-      this.image,
-      this.images,
-      this.inCart,
-      this.inFavorites,
-      this.name,
-      this.oldPrice,
-      this.price});
-
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-      description: json['description'],
-      discount: json['discount'],
-      id: json['id'],
-      image: json['image'],
-      images: json['images'] != null ? List<String>.from(json['images']) : null,
-      inCart: json['in_cart'],
-      inFavorites: json['in_favorites'],
-      name: json['name'],
-      oldPrice: json['old_price'],
-      price: json['price'],
-    );
+  Product.fromJson(Map<String, dynamic> json) {
+    description = json['description'];
+    discount = json['discount'];
+    id = json['id'];
+    image = json['image'];
+    images = json['images'] != null ? List<String>.from(json['images']) : null;
+    inCart = json['in_cart'];
+    inFavorites = json['in_favorites'];
+    name = json['name'];
+    oldPrice = json['old_price'];
+    price = json['price'];
   }
 }
