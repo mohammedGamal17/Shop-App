a0078e2d3f3649a60e29aac0ad959266072d7489
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-24 09:44:44 +0200

    Update product.dart

--- lib/models/get_fav_model/product.dart
+++ lib/models/get_fav_model/product.dart
@@ -1,30 +1,21 @@
 class Product {
   String? description;
   dynamic discount;
-  int? id;
+  late int id;
   String? image;
   String? name;
   dynamic oldPrice;
   dynamic price;
 
-  Product(
-      {this.description,
-      this.discount,
-      this.id,
-      this.image,
-      this.name,
-      this.oldPrice,
-      this.price});
 
-  factory Product.fromJson(Map<String, dynamic> json) {
-    return Product(
-      description: json['description'],
-      discount: json['discount'],
-      id: json['id'],
-      image: json['image'],
-      name: json['name'],
-      oldPrice: json['old_price'],
-      price: json['price'],
-    );
+   Product.fromJson(Map<String, dynamic> json) {
+      description= json['description'];
+      discount= json['discount'];
+      id= json['id'];
+      image= json['image'];
+      name= json['name'];
+      oldPrice= json['old_price'];
+      price= json['price'];
+
   }
 }
