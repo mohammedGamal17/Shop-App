974fdc50324e2d2a15d4a841f7ec844a31d9bc76
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-25 09:51:03 +0200

    Create data.dart

---
+++ lib/models/favorites/get_fav_model/data.dart
@@ -0,0 +1,47 @@
+import 'package:shop_app/models/favorites/get_fav_model/data_x.dart';
+
+class Data {
+  List<DataX>? data = [];
+  int? currentPage;
+  String? firstPageUrl;
+  int? from;
+  int? lastPage;
+  String? lastPageUrl;
+  Object? nextPageUrl;
+  String? path;
+  int? perPage;
+  Object? prevPageUrl;
+  int? to;
+  dynamic total;
+
+  Data(
+      {this.data,
+      this.currentPage,
+      this.firstPageUrl,
+      this.from,
+      this.lastPage,
+      this.lastPageUrl,
+      this.nextPageUrl,
+      this.path,
+      this.perPage,
+      this.prevPageUrl,
+      this.to,
+      this.total});
+
+  factory Data.fromJson(Map<String, dynamic> json) {
+    return Data(
+      data: json['data'] != null
+          ? (json['data'] as List).map((i) => DataX.fromJson(i)).toList()
+          : null,
+      currentPage: json['current_page'],
+      firstPageUrl: json['first_page_url'],
+      from: json['from'],
+      lastPage: json['last_page'],
+      lastPageUrl: json['last_page_url'],
+      path: json['path'],
+      perPage: json['per_page'],
+      to: json['to'],
+      total: json['total'],
+    );
+  }
+}
