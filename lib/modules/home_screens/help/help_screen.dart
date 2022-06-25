96adf0dd068508a5cdbb717e944f5d4e95ed21a2
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-25 15:16:50 +0200

    Update help_screen.dart

--- lib/modules/home_screens/help/help_screen.dart
+++ lib/modules/home_screens/help/help_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
+import 'package:shop_app/modules/Login/login.dart';
 import 'package:shop_app/shared/cubit/cubit.dart';
-
 import '../../../main.dart';
 import '../../../shared/components/components.dart';
 import '../../../shared/cubit/states.dart';
@@ -16,28 +17,60 @@ class HelpScreen extends StatelessWidget {
     return BlocProvider(
       create: (context) => AppCubit(),
       child: BlocConsumer<AppCubit, AppStates>(
-        listener: (context, state) {},
+        listener: (context, state) {
+
+        },
         builder: (context, state) {
           return Scaffold(
             body: Center(
               child: Column(
                 children: [
-                  Text('Help Screen',style: Theme.of(context).textTheme.bodyText1),
-                  IconButton(
-                      onPressed: () {
-                        sharedPreferences.remove('token').then((value) {
-                          sharedPreferences.remove('fakeId');
-                          navigateToAndReplace(
-                            context,
-                            const FirstScreen(),
-                          );
-                        }).catchError((onError) {
-                          if (kDebugMode) {
-                            print(onError.toString());
-                          }
-                        });
-                      },
-                      icon: const Icon(Icons.logout_outlined)),
+                  Text('Help Screen',
+                      style: Theme.of(context).textTheme.bodyText1),
+                  sharedPreferences.getString('token') != null
+                      ? Column(
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  sharedPreferences
+                                      .remove('fakeId')
+                                      .then((value) async {
+                                    sharedPreferences.remove('token');
+                                    navigateToAndReplace(
+                                      context,
+                                      const FirstScreen(),
+                                    );
+
+                                  }).catchError((onError) {
+                                    if (kDebugMode) {
+                                      print(onError.toString());
+                                    }
+                                  });
+                                },
+                                icon: const Icon(Icons.logout_outlined)),
+                            Text(
+                              'logout'.capitalize!,
+                              style: Theme.of(context).textTheme.bodyText1,
+                            )
+                          ],
+                        )
+                      : Column(
+                          children: [
+                            IconButton(
+                              onPressed: () {
+                                navigateToAndReplace(
+                                  context,
+                                  Login(),
+                                );
+                              },
+                              icon: const Icon(Icons.login_outlined),
+                            ),
+                            Text(
+                              'login'.capitalize!,
+                              style: Theme.of(context).textTheme.bodyText1,
+                            )
+                          ],
+                        )
                 ],
               ),
             ),
