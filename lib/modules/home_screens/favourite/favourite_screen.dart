61409d192581112193c7aeb0f24ac55f15f3c58b
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-25 15:16:42 +0200

    Update favourite_screen.dart

--- lib/modules/home_screens/favourite/favourite_screen.dart
+++ lib/modules/home_screens/favourite/favourite_screen.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:shop_app/models/favorites/get_fav_model/data_x.dart';
+import 'package:shop_app/modules/Login/login.dart';
 
 import '../../../shared/components/components.dart';
 import '../../../shared/cubit/cubit.dart';
@@ -17,8 +19,8 @@ class FavouriteScreen extends StatelessWidget {
       create: (context) => AppCubit()..getFavData(),
       child: BlocConsumer<AppCubit, AppStates>(
         listener: (context, state) {
-          if(state is FavSuccessState){
-            if(!state.model.status!){
+          if (state is FavSuccessState) {
+            if (!state.model.status!) {
               snack(context, content: state.model.message!);
             }
           }
@@ -26,17 +28,16 @@ class FavouriteScreen extends StatelessWidget {
         builder: (context, state) {
           AppCubit cubit = AppCubit.get(context);
           return Scaffold(
-            body: state is ! GetFavLoadingState
-                ? ListView.separated(
-                    physics: const BouncingScrollPhysics(),
-                    itemBuilder: (context, index) => itemBuilder(
-                        cubit.getFavModel!.data!.data![index], context),
-                    separatorBuilder: (context, index) =>
-                        const SizedBox(height: 0.0),
-                    itemCount: cubit.getFavModel!.data!.data!.length,
-                  )
-                : circularProgressIndicator()
-          );
+              body: state is! GetFavLoadingState
+                  ? ListView.separated(
+                      physics: const BouncingScrollPhysics(),
+                      itemBuilder: (context, index) => itemBuilder(
+                          cubit.getFavModel!.data!.data![index], context),
+                      separatorBuilder: (context, index) =>
+                          const SizedBox(height: 0.0),
+                      itemCount: cubit.getFavModel!.data!.data!.length,
+                    )
+                  : noItemFounded(context));
         },
       ),
     );
@@ -68,21 +69,21 @@ class FavouriteScreen extends StatelessWidget {
                 alignment: AlignmentDirectional.bottomStart,
                 children: [
                   Image(
-                    image: NetworkImage('${model.product!.image}'),
+                    image: NetworkImage('${model.product?.image}'),
                     height: 125.0,
                     width: 125.0,
                     fit: BoxFit.fill,
                   ),
                   Row(
                     children: [
-                      model.product!.discount != 0
+                      model.product?.discount != 0
                           ? Container(
                               color: Colors.red,
                               child: Padding(
                                 padding:
                                     const EdgeInsets.symmetric(horizontal: 7.0),
                                 child: Text(
-                                  '% ${model.product!.discount}',
+                                  '% ${model.product?.discount}',
                                   style: const TextStyle(
                                     color: Colors.white,
                                     fontSize: 10.0,
@@ -104,7 +105,7 @@ class FavouriteScreen extends StatelessWidget {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     Text(
-                      '${model.product!.name}',
+                      '${model.product?.name}',
                       style: TextStyle(
                         color: HexColor('0077B6'),
                         fontFamily: 'Changa',
@@ -116,7 +117,7 @@ class FavouriteScreen extends StatelessWidget {
 
                     ///if discount = 0 hide it
                     Text(
-                      'EGP ${model.product!.oldPrice}',
+                      'EGP ${model.product?.oldPrice}',
                       style: TextStyle(
                         color: HexColor('0077B6'),
                         fontFamily: 'Changa',
@@ -126,7 +127,7 @@ class FavouriteScreen extends StatelessWidget {
                     const Spacer(),
                     Row(
                       children: [
-                        if (model.product!.discount != 0)
+                        if (model.product?.discount != 0)
                           Text(
                             'EGP 2000',
                             style: TextStyle(
@@ -142,19 +143,18 @@ class FavouriteScreen extends StatelessWidget {
                             AppCubit.get(context)
                                 .postFavData(model.product!.id);
                           },
-                          icon:
-                              AppCubit.get(context).favMap[model.product!.id] !=
-                                      true
-                                  ? Icon(
-                                      AppCubit.get(context).icon =
-                                          Icons.favorite,
-                                      color: HexColor('0077B6'),
-                                    )
-                                  : Icon(
-                                      AppCubit.get(context).icon =
-                                          Icons.favorite_border_outlined,
-                                      color: HexColor('0077B6'),
-                                    ),
+                          icon: AppCubit.get(context)
+                                      .favMap[model.product?.id] !=
+                                  true
+                              ? Icon(
+                                  AppCubit.get(context).icon = Icons.favorite,
+                                  color: HexColor('0077B6'),
+                                )
+                              : Icon(
+                                  AppCubit.get(context).icon =
+                                      Icons.favorite_border_outlined,
+                                  color: HexColor('0077B6'),
+                                ),
                         ),
                       ],
                     ),
@@ -164,6 +164,32 @@ class FavouriteScreen extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget noItemFounded(context) {
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+              onPressed: () {
+                navigateToAndReplace(context, Login());
+              },
+              icon: Icon(
+                Icons.login,
+                color: Colors.grey[200],
+              )),
+          const SizedBox(
+            height: 10.0,
+          ),
+          Text(
+            'please login ${Login()}'.capitalize!,
+            style: Theme.of(context).textTheme.bodyText1,
+          )
+        ],
       ),
     );
   }
