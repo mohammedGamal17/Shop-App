edcb3e50b8c01bfc4aab08dbe18d2e2707696664
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-25 16:45:45 +0200

    Update data_storage.dart

--- lib/shared/network/local/data_storage.dart
+++ lib/shared/network/local/data_storage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:get/get_core/src/get_main.dart';
+
 import 'package:get_storage/get_storage.dart';
 
 class DataStorage {
