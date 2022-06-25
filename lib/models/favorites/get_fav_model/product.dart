db5edb3f1cfcd312f68fcaf0afdfcd7bc6463b51
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-25 09:51:10 +0200

    Create product.dart

---
+++ lib/models/favorites/get_fav_model/product.dart
@@ -0,0 +1,21 @@
+class Product {
+  String? description;
+  dynamic discount;
+  late int id;
+  String? image;
+  String? name;
+  dynamic oldPrice;
+  dynamic price;
+
+
+   Product.fromJson(Map<String, dynamic> json) {
+      description= json['description'];
+      discount= json['discount'];
+      id= json['id'];
+      image= json['image'];
+      name= json['name'];
+      oldPrice= json['old_price'];
+      price= json['price'];
+
+  }
+}
