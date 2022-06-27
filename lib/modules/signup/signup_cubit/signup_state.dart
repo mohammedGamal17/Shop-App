9ba8c891060a74ec21cd8b2915e6c9270b890bbe
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-27 06:39:39 +0200

    Update signup_state.dart

--- lib/modules/signup/signup_cubit/signup_state.dart
+++ lib/modules/signup/signup_cubit/signup_state.dart
@@ -1,10 +1,16 @@
+import 'package:shop_app/models/user/register/register.dart';
+
 abstract class SignUpStates {}
 
 class SignUpInitial extends SignUpStates {}
 
 class SignUpLoadingState extends SignUpStates {}
 
-class SignUpSuccessState extends SignUpStates {}
+class SignUpSuccessState extends SignUpStates {
+  final RegisterModel registerModel;
+
+  SignUpSuccessState(this.registerModel);
+}
 
 class SignUpFailState extends SignUpStates {
   final String error;
