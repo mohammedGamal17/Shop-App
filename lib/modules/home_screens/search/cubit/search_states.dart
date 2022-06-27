208a84e5657c55f78bc35f97a7cff7503e97acca
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-27 11:40:20 +0200

    Create search_states.dart

---
+++ lib/modules/home_screens/search/cubit/search_states.dart
@@ -0,0 +1,9 @@
+abstract class SearchStates{}
+
+class SearchInitState extends SearchStates{}
+
+class SearchLoadingState extends SearchStates{}
+
+class SearchSuccessState extends SearchStates{}
+
+class SearchFailState extends SearchStates{}
