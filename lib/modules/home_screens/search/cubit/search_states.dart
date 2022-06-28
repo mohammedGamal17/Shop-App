9f5e38c8b3fe24bdf8c3ffb173732f686f36d113
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-28 10:02:12 +0200

    Delete search_states.dart

--- lib/modules/home_screens/search/cubit/search_states.dart
+++
@@ -1,9 +0,0 @@
-abstract class SearchStates{}
-
-class SearchInitState extends SearchStates{}
-
-class SearchLoadingState extends SearchStates{}
-
-class SearchSuccessState extends SearchStates{}
-
-class SearchFailState extends SearchStates{}
