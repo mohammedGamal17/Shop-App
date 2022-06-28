45b2c15a2ac210d530a6f9ae50c41029f0f5edf1
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-28 11:51:55 +0200

    Update assets_res.dart

--- lib/res/assets_res.dart
+++ lib/res/assets_res.dart
@@ -3,5 +3,8 @@
 // ignore_for_file: constant_identifier_names
 // ignore_for_file: lines_longer_than_80_chars
 class AssetsRes {
+  static const String LOGO = 'assets/Images/logo.png';
   static const String PAGEVIEW = 'assets/Images/pageview.png';
+  static const String PAGEVIEW2 = 'assets/Images/pageview2.png';
+  static const String PAGEVIEW3 = 'assets/Images/pageview3.png';
 }
