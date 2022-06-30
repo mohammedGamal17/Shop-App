8c5036e28ecb363aee11c645f37f2fcf3f0c1cfb
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-30 20:32:19 +0200

    Update search_screen.dart

--- lib/modules/home_screens/search/search_screen.dart
+++ lib/modules/home_screens/search/search_screen.dart
@@ -75,119 +75,108 @@ class SearchScreen extends StatelessWidget {
   Widget itemBuilder(DataX model, context, {bool isOldPrice = true}) {
     return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: Container(
-        height: 130.0,
-        width: double.infinity,
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(10.0),
-          color: buttonColor,
-          gradient: LinearGradient(
-            begin: AlignmentDirectional.topStart,
-            end: AlignmentDirectional.bottomEnd,
-            colors: [
-              HexColor('FFFFFF'),
-              HexColor('FFFFFF'),
-            ],
+      child: InkWell(
+        onTap: (){
+        },
+        child: Container(
+          height: 130.0,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10.0),
+            color: buttonColor,
+            gradient: LinearGradient(
+              begin: AlignmentDirectional.topStart,
+              end: AlignmentDirectional.bottomEnd,
+              colors: [
+                HexColor('FFFFFF'),
+                HexColor('FFFFFF'),
+              ],
+            ),
           ),
-        ),
-        child: Padding(
-          padding: const EdgeInsets.all(5.0),
-          child: Row(
-            children: [
-              Stack(
-                alignment: AlignmentDirectional.bottomStart,
-                children: [
-                  Image(
-                    image: NetworkImage(model.image),
-                    height: 125.0,
-                    width: 125.0,
-                    fit: BoxFit.fill,
-                  ),
-                  Row(
-                    children: [
-                      model.discount != 0 && isOldPrice
-                          ? Container(
-                              color: Colors.red,
-                              child: Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 7.0),
-                                child: Text(
-                                  '% ${model.discount}',
-                                  style: const TextStyle(
-                                    color: Colors.white,
-                                    fontSize: 10.0,
-                                  ),
-                                ),
-                              ),
-                            )
-                          : Container(),
-                    ],
-                  ),
-                ],
-              ),
-              const SizedBox(
-                width: 20.0,
-              ),
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
+          child: Padding(
+            padding: const EdgeInsets.all(5.0),
+            child: Row(
+              children: [
+                Stack(
+                  alignment: AlignmentDirectional.bottomStart,
                   children: [
-                    Text(
-                      model.name,
-                      style: TextStyle(
-                        color: HexColor('0077B6'),
-                        fontFamily: 'Changa',
-                        fontSize: 12.0,
-                      ),
-                      maxLines: 1,
-                      overflow: TextOverflow.ellipsis,
+                    Image(
+                      image: NetworkImage(model.image),
+                      height: 125.0,
+                      width: 125.0,
+                      fit: BoxFit.fill,
                     ),
-
-                    ///if discount = 0 hide it
-                    Text(
-                      'EGP ${model.price}',
-                      style: TextStyle(
-                        color: HexColor('0077B6'),
-                        fontFamily: 'Changa',
-                        fontSize: 14.0,
-                      ),
-                    ),
-                    const Spacer(),
                     Row(
                       children: [
-                        if (model.discount != 0 && isOldPrice)
-                          Text(
-                            '${model.oldPrice}',
-                            style: TextStyle(
-                              color: HexColor('90E0EF'),
-                              fontFamily: 'Changa',
-                              fontSize: 10.0,
-                              decoration: TextDecoration.lineThrough,
-                            ),
-                          ),
-                        const Spacer(),
-                        IconButton(
-                          onPressed: () {
-                            AppCubit.get(context).postFavData(model.id);
-                          },
-                          icon: AppCubit.get(context).favMap[model.id] == true
-                              ? Icon(
-                                  AppCubit.get(context).icon = Icons.favorite,
-                                  color: HexColor('0077B6'),
-                                )
-                              : Icon(
-                                  AppCubit.get(context).icon =
-                                      Icons.favorite_border_outlined,
-                                  color: HexColor('0077B6'),
+                        model.discount != 0 && isOldPrice
+                            ? Container(
+                                color: Colors.red,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.symmetric(horizontal: 7.0),
+                                  child: Text(
+                                    '% ${model.discount}',
+                                    style: const TextStyle(
+                                      color: Colors.white,
+                                      fontSize: 10.0,
+                                    ),
+                                  ),
                                 ),
-                        ),
+                              )
+                            : Container(),
                       ],
                     ),
                   ],
                 ),
-              ),
-            ],
+                const SizedBox(
+                  width: 20.0,
+                ),
+                Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Text(
+                        model.name,
+                        style: TextStyle(
+                          color: HexColor('0077B6'),
+                          fontFamily: 'Changa',
+                          fontSize: 12.0,
+                        ),
+                        maxLines: 1,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+
+                      ///if discount = 0 hide it
+                      Text(
+                        'EGP ${model.price}',
+                        style: TextStyle(
+                          color: HexColor('0077B6'),
+                          fontFamily: 'Changa',
+                          fontSize: 14.0,
+                        ),
+                      ),
+                      const Spacer(),
+                      Row(
+                        children: [
+                          if (model.discount != 0 && isOldPrice)
+                            Text(
+                              '${model.oldPrice}',
+                              style: TextStyle(
+                                color: HexColor('90E0EF'),
+                                fontFamily: 'Changa',
+                                fontSize: 10.0,
+                                decoration: TextDecoration.lineThrough,
+                              ),
+                            ),
+                          const Spacer(),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
