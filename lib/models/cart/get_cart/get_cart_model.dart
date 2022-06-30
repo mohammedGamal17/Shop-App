e803fb591a3f0683e5636d9022e4893527dbc23e
mohammedGamal7 <mohammed_gamal7@outlook.com>
2022-06-30 17:12:34 +0200

    Update get_cart_model.dart

--- lib/models/cart/get_cart/get_cart_model.dart
+++ lib/models/cart/get_cart/get_cart_model.dart
@@ -1,25 +1,28 @@
 import 'package:shop_app/models/cart/get_cart/data.dart';
 
 class GetCartModel {
-    Data ?data;
-   late bool status;
+  Data? data;
+  late bool status;
 
-    GetCartModel({this.data, required this.status,});
+  GetCartModel({
+    this.data,
+    required this.status,
+  });
 
-    factory GetCartModel.fromJson(Map<String, dynamic> json) {
-        return GetCartModel(
-            data: json['data'] != null ? Data.fromJson(json['data']) : null,
-            status: json['status'], 
-        );
+  factory GetCartModel.fromJson(Map<String, dynamic> json) {
+    return GetCartModel(
+      data: json['data'] != null ? Data.fromJson(json['data']) : null,
+      status: json['status'],
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> dataX = new Map<String, dynamic>();
+    dataX['status'] = this.status;
+    final data = this.data;
+    if (data != null) {
+      dataX['data'] = data.toJson();
     }
-
-    Map<String, dynamic> toJson() {
-        final Map<String, dynamic> dataX = new Map<String, dynamic>();
-        dataX['status'] = this.status;
-        final data = this.data;
-        if (data != null) {
-            dataX['data'] = data.toJson();
-        }
-        return dataX;
-    }
+    return dataX;
+  }
 }
